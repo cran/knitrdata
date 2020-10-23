cf4178d353b664511b482a4b9318cc335bfae115
David M. Kaplan <dmkaplan2000@gmail.com>
2020-10-23 20:10:02 +0000

    version 0.5.2

--- R/utils.R
+++ R/utils.R
@@ -20,9 +20,26 @@
 #' @return A boolean that will be \code{TRUE} if a file is considered to be binary.
 #'
 #' @author David M. Kaplan \email{dmkaplan2000@@gmail.com}
+#' @export
 is.file.binary = function(file,bin.chars=c(1:8,14:25),nchars=1000,nbin=2) {
   x = as.integer(readBin(file,"raw",nchars))
   n = sum(x %in% bin.chars)
 
   return(n>nbin)
 }
+
+# Helper function for handling text file newline mess --------
+
+#' Platform independent newline string
+#'
+#' A simple function to determine the appropriate newline string for a given
+#' operating system.
+#'
+#' @param os Name of the operating system. Defaults to \code{.Platform$OS.type}
+#'
+#' @return For Windows, this should return \code{'\r\n'}, whereas for
+#' other operating system it will return \code{'\n'}.
+#'
+#' @author David M. Kaplan \email{dmkaplan2000@@gmail.com}
+#' @export
+platform.newline = function(os=.Platform$OS.type) ifelse(grepl("windows",tolower(os)),"\r\n","\n")
